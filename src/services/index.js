d33546f8bbe78d7c371bf411a4d989152ad3c722
Krisrp <kristoforusrp@gmail.com>
2018-03-20 15:53:45 +0700

    Add family service

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const registerUser = require('./register/register.service.js');
 const accessToken = require('./access-tokens/access.tokens.service.js');
+const family = require('./user-family/users.family.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(users);
   app.configure(registerUser);
   app.configure(accessToken);
+  app.configure(family);
 };
