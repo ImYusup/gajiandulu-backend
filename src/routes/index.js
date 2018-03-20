7e22a1d2a27457406f48d53abf28a2b06e9ae45d
Krisrp <kristoforusrp@gmail.com>
2018-03-21 02:01:00 +0700

    Add route and validation

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const register = require('./register');
+const login = require('./login');
+const users = require('./users');
+const family = require('./families');
+
+module.exports = {
+  register,
+  users,
+  family,
+  login
+};
