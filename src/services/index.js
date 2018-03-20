df53723d3ca7f64045b3d4a7c8d6f413733db091
Dhanial Rizky <activatedfor@gmail.com>
2018-03-20 17:20:43 +0700

    feature(identity-card) #3: create end-points for identity cards

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const registerUser = require('./register/register.service.js');
 const accessToken = require('./access-tokens/access.tokens.service.js');
 const occupations = require('./occupations/occupations.service.js');
+const identityCards = require('./identity-cards/identity.cards.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(users);
   app.configure(registerUser);
   app.configure(accessToken);
   app.configure(occupations);
+  app.configure(identityCards);
 };
