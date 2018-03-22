3a4ca2b3befb0d32ce377f5069b63c94624116af
Krisrp <kristoforusrp@gmail.com>
2018-03-22 16:08:49 +0700

    Cleaned up

--- src/services/index.js
+++ src/services/index.js
@@ -3,11 +3,13 @@ const accessTokenService = require('./access-tokens/access.tokens.service.js');
 const familyService = require('./families/families.service.js');
 const identityCardService = require('./identity-cards/identity.cards.service');
 const occupationService = require('./occupations/occupations.service');
+const digitalAssetService = require('./digital-assets.js/digital.assets.service');
 // eslint-disable-next-line no-unused-vars
 module.exports = {
   userService,
   familyService,
   accessTokenService,
   identityCardService,
-  occupationService
+  occupationService,
+  digitalAssetService
 };
