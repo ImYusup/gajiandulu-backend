4e4dfff837beaf5e46732a6f8aff0e6120d39c3e
hudabikhoir <hudaparodi@gmail.com>
2018-04-11 17:11:27 +0700

    validation registration complete

--- src/services/forgot-password/forgot.password.service.js
+++ src/services/forgot-password/forgot.password.service.js
@@ -28,43 +28,53 @@ const forgotPasswordService = {
 
       // crypt.compareSync(hash, user.hash)
       if (hash === user.hash) {
-        var auth = {
-          auth: {
-            api_key: 'key-4d9b63aeae8c8f075d1ab37c273963d5',
-            domain: 'sandboxe207aeefc40140019bd222b370bff1ca.mailgun.org'
-          },
-          proxy: 'http://root:@localhost:3000' // optional proxy, default is false
-        }
-        
-        var nodemailerMailgun = nodemailer.createTransport(mg(auth));
-        
-        nodemailerMailgun.sendMail({
-          from: 'hudaparodi@example.com',
-          to: 'hudaparodi@gmail.com', // An array if you have multiple recipients.
-          subject: 'Password Reset - GAJIANDULU',
-          'h:Reply-To': 'reply2this@company.com',
-          //You can use "html:" to send HTML email content. It's magic!
-          html: '<b>Password reset succesfully.</b> Your new password is ' + passgen,
-        }, function (err, info) {
-          if (err) {
-            console.log('Error: ' + err);
+        if(user.registration_complete == 1){
+          var auth = {
+            auth: {
+              api_key: 'key-4d9b63aeae8c8f075d1ab37c273963d5',
+              domain: 'sandboxe207aeefc40140019bd222b370bff1ca.mailgun.org'
+            },
+            proxy: 'http://root:@localhost:3000' // optional proxy, default is false
           }
-          else {
-            console.log('Response: ' + info);
-          }
-        });
-        await User.update(
-          {
-            password: hashPassword
-          },
-          { where: { email: email, hash: hash } }
-        );
-        return res
+          
+          var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+          
+          nodemailerMailgun.sendMail({
+            from: 'hudaparodi@example.com',
+            to: 'hudaparodi@gmail.com', // An array if you have multiple recipients.
+            subject: 'Password Reset - GAJIANDULU',
+            'h:Reply-To': 'reply2this@company.com',
+            //You can use "html:" to send HTML email content. It's magic!
+            html: '<b>Password reset succesfully.</b> Your new password is ' + passgen,
+          }, function (err, info) {
+            if (err) {
+              console.log('Error: ' + err);
+            }
+            else {
+              console.log('Response: ' + info);
+            }
+          });
+          await User.update(
+            {
+              password: hashPassword
+            },
+            { where: { email: email, hash: hash } }
+          );
+          return res
           .status(200)
           .json(
             response(
               true,
               'Password successfully reset, please check your email'
+            )
+          );
+        }
+        return res
+          .status(200)
+          .json(
+            response(
+              true,
+              'You must complete your registration'
             )
           );
       }
