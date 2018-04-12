45f284b9b8acc6e5d87d53d575085e16e5dfa278
rizalreza <rizalrezamaulana.rrm@gmail.com>
2018-04-12 19:20:29 +0700

    #17 complete all queries and sub without mutation

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,9 +1,12 @@
 const { createAdmin, updateAdmin, deleteAdmin } = require('./admin-management-mutations');
 const { updateLoan, deleteLoan } = require('./loans-mutations');
+const updateUser = require('./user/update');
+
 module.exports = {
   createAdmin,
   updateAdmin,
   deleteAdmin,
   updateLoan,
-  deleteLoan
+  deleteLoan,
+  updateUser
 };
