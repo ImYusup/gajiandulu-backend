e17d4b827bed7be3619ba61dcf92037915b83722
Dhanial Rizky <activatedfor@gmail.com>
2018-04-12 18:23:12 +0700

    completed the admin spec mechanism

--- src/graphql/mutations/admin-management-mutations.js
+++ src/graphql/mutations/admin-management-mutations.js
@@ -1,20 +1,18 @@
-require("module-alias/register");
+require('module-alias/register');
 const {
-  GraphQLList,
   GraphQLNonNull,
-  GraphQLInt,
   GraphQLString,
   GraphQLID
-} = require("graphql");
+} = require('graphql');
 
-const { response } = require("@helpers");
-const crypt = require("bcrypt");
-const { users: UserModel } = require("@models");
-const UserType = require("../types/users");
+const { response } = require('@helpers');
+const crypt = require('bcrypt');
+const { users: UserModel } = require('@models');
+const UserType = require('../types/users');
 
 const createAdmin = {
   type: UserType,
-  description: "create admin",
+  description: 'create admin',
   args: {
     full_name: {
       type: GraphQLNonNull(GraphQLString)
@@ -25,7 +23,7 @@ const createAdmin = {
     phone: {
       type: GraphQLNonNull(GraphQLString)
     },
-    birthday: {
+    date_of_birth: {
       type: GraphQLNonNull(GraphQLString)
     },
     password: {
@@ -33,7 +31,7 @@ const createAdmin = {
     }
   },
   async resolve(root, args) {
-    const { full_name, email, phone, birthday, password } = args;
+    const { full_name, email, phone, date_of_birth, password } = args;
 
     const hashPassword = crypt.hashSync(password, 15);
     const hash = crypt.hashSync(new Date().toString() + email, 10);
@@ -43,7 +41,7 @@ const createAdmin = {
         full_name,
         email,
         password: hashPassword,
-        date_of_birth: birthday,
+        date_of_birth,
         phone,
         hash,
         is_confirmed_email: 1,
@@ -53,71 +51,60 @@ const createAdmin = {
     );
     // YOU SHOULD remember that we only to create admin data, there should be role_id = 1
     const results = await UserModel.create(dataInsert);
-    return results;
+    if (results) {
+      return results;
+    } else {
+      return root
+        .status(400)
+        .json(response(false, 'failed to insert to database'));
+    }
   }
 };
 
 // const updateUdmin
 const updateAdmin = {
   type: UserType,
-  description: "update admin",
+  description: 'update admin',
   args: {
     id: {
       type: GraphQLNonNull(GraphQLID)
     },
     full_name: {
-      type: GraphQLNonNull(GraphQLString)
+      type: GraphQLString
     },
     email: {
-      type: GraphQLNonNull(GraphQLString)
+      type: GraphQLString
     },
     phone: {
-      type: GraphQLNonNull(GraphQLString)
+      type: GraphQLString
     },
-    birthday: {
-      type: GraphQLNonNull(GraphQLString)
+    date_of_birth: {
+      type: GraphQLString
     },
     password: {
-      type: GraphQLNonNull(GraphQLString)
+      type: GraphQLString
     }
   },
 
   async resolve(root, args) {
-    const { full_name, email, phone, birthday, password } = args;
-
-    const hashPassword = crypt.hashSync(password, 15);
-    const hash = crypt.hashSync(new Date().toString() + email, 10);
-    const dataUpdate = Object(
-      {},
-      {
-        full_name,
-        email,
-        password: hashPassword,
-        date_of_birth: birthday,
-        phone,
-        hash,
-        is_confirmed_email: 1,
-        role_id: 1,
-        registration_complete: 1
-      }
-    );
     // YOU SHOULD remember that we only to create admin data, there should be role_id = 1
-    const update = await UserModel.findById(args.id).then(result => {
-      return result.update({
-        full_name,
-        email,
-        phone,
-        birthday,
-        password
-      });
+    return await UserModel.findById(args.id).then((result, error) => {
+      if (result) {
+        const hash = crypt.hashSync(new Date().toString() + args.email || result.dataValues.email, 10);
+        const data = Object.assign({}, args, { hash: hash });
+        return result.update(data);
+      } else {
+        return root
+          .status(400)
+          .json(response(false, 'admin id not found', error));
+      }
     });
-    return update;
   }
 };
 
 const deleteAdmin = {
   type: UserType,
-  description: "delete admin",
+  description: 'delete admin',
   args: {
     id: {
       type: GraphQLNonNull(GraphQLID)
@@ -127,7 +114,7 @@ const deleteAdmin = {
     // YOU SHOULD remember that we only to create admin data, there should be role_id = 1
     const destroy = await UserModel.destroy({ where: { id: args.id } });
     if (destroy) {
-      root.status(200).json(response(true, "admin deleted successfully"));
+      root.status(200).json(response(true, 'admin deleted successfully'));
     }
   }
 };
--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,6 +1,6 @@
-const { createAdmin } = require("./admin-management-mutations");
-const { updateAdmin } = require("./admin-management-mutations");
-const { deleteAdmin } = require("./admin-management-mutations");
+const { createAdmin } = require('./admin-management-mutations');
+const { updateAdmin } = require('./admin-management-mutations');
+const { deleteAdmin } = require('./admin-management-mutations');
 module.exports = {
   createAdmin,
   updateAdmin,
