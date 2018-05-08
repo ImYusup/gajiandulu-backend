32ae8c0e1cde4ca05f078fa965cb61e9553bf6fd
Rahmat Hidayat <rh.hie.dayatt@gmail.com>
2018-05-08 21:39:14 +0700

    #39-WIP

---
+++ src/models/notifications.model.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var notifications = sequelize.define('notifications', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    employee_id: {
+      type: DataTypes.INTEGER
+    },
+    body: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    created_at: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updated_at: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {
+    underscored: true,
+  });
+  notifications.associate = function(models) {
+    // associations can be defined here
+  };
+  return notifications;
+};
