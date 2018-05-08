2c4b1580a7835f72b977677e83427aa3c51bb613
riaanggraini99 <ria.anggraini@students.amikom.ac.id>
2018-05-08 20:02:49 +0700

    #37-Create-Migration-and-Model-for-Presences-Table

---
+++ src/database/migrations/20180508122725-create-presences.js
@@ -0,0 +1,78 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('presences', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      employee_id: {
+        allowNull: true,
+        foreignKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'employees',
+          key: 'id'
+        },
+        defaultValue: 2
+      },
+      presence_date: {
+        allowNull: false,
+        type: Sequelize.DATEONLY
+      },
+      presence_start: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      presence_end: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      rest_start: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      rest_end: {
+        allowNull: false,
+        type:Sequelize.DATE
+      },
+      is_absence: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      is_leave: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      overwork: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      work_hours: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      salary: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      fine: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('presences');
+  }
+};
