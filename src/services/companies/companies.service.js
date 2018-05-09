3128f7ed78a0a504311a78fd44cab6252f0f0be3
Dhanial Rizky <activatedfor@gmail.com>
2018-05-09 21:56:05 +0700

    make some LIKE query

--- src/services/companies/companies.service.js
+++ src/services/companies/companies.service.js
@@ -1,6 +1,8 @@
 require('module-alias/register');
 const { response } = require('@helpers');
 const { companies: Company } = require('@models');
+const Sequelize = require('sequelize');
+const { Op } = Sequelize;
 
 const companyService = {
   create: async (req, res) => {
@@ -10,34 +12,44 @@ const companyService = {
     // let noSpaces = req.body.name.replace(/\s/g, '');
     // let theCode = noSpaces.replace(/[aeiou]/ig,'').toUpperCase();
     let theCode = 'PTBB';
-    let lastNum = '001';
+    // let lastNum = '001';
     // let finalCode = (theCode+lastNum);
     const companyExist = await Company.findOne({
-      where: ['codename like ?', `${theCode}%` + '%']
+      where: { codename: { [Op.like]: `${theCode}%` } }
     });
-    /* eslint-disable*/
-    console.log('datanya', companyExist);
+    /* eslint-disable no-console*/
+    console.log('datanya', parseInt(companyExist.codename.substr(-3)) + 1);
 
-    if (companyExist) {
-      let lastNums = companyExist.substr(-3);
-      lastNums = parseInt(lastNums);
-      lastNums++;
-      lastNums = ('0000'+lastNums).substr(-3);
-      let finalCode = (theCode+lastNums);
-    } else {
-      lastNum = parseInt(lastNum);
-      // lastNum++;
-      lastNum = ('0000'+lastNum).substr(-3);
-      let finalCode = (theCode+lastNum);
-    }
+    // if (companyExist) {
+    //   let lastNums = companyExist.substr(-3);
+    //   lastNums = parseInt(lastNums);
+    //   lastNums++;
+    //   lastNums = ('0000' + lastNums).substr(-3);
+    //   let finalCode = theCode + lastNums;
+    // } else {
+    //   lastNum = parseInt(lastNum);
+    //   // lastNum++;
+    //   lastNum = ('0000' + lastNum).substr(-3);
+    //   let finalCode = theCode + lastNum;
+    // }
 
     try {
-      const payload = Object.assign({}, data, {codename: finalCode, active: true} );
+      const payload = Object.assign({}, data, {
+        codename: finalCode,
+        active: true
+      });
       let company = await Company.create(payload);
       if (company) {
         return res
           .status(200)
-          .json(response(true, 'Company has been successfully created', company, null));
+          .json(
+            response(
+              true,
+              'Company has been successfully created',
+              company,
+              null
+            )
+          );
       }
     } catch (error) {
       if (error.errors) {
