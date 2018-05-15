b08ee700b6fb67feb8a4f7cd55c70a87b7102ab2
shafrianadhi <shafrianadhi@gmail.com>
2018-05-15 19:43:46 +0700

    #47-hot-fix

--- src/services/v1/dashboard/dashboard.service.js
+++ src/services/v1/dashboard/dashboard.service.js
@@ -1,9 +1,6 @@
 require('module-alias/register');
 const { response } = require('@helpers');
-const {
-  journals: Journals,
-  employees: Employee,
-} = require('@models');
+const { journals: Journals, employees: Employee } = require('@models');
 const Sequelize = require('sequelize');
 
 const dashboardService = {
@@ -15,36 +12,52 @@ const dashboardService = {
     try {
       const total_salary = await Employee.findAll({
         where: { company_id: companyId },
-        attributes: [[Sequelize.fn('SUM', Sequelize.col('salary')), 'total_salaries']],
+        attributes: [
+          [Sequelize.fn('SUM', Sequelize.col('salary')), 'total_salaries']
+        ]
       });
       const deposits = await Employee.findAll({
         attributes: ['company_id'],
         where: { company_id: companyId },
         raw: true,
-        include: [{
-          model: Journals,
-          attributes: [[Sequelize.fn('SUM', (Sequelize.col('debet'), Sequelize.literal('-'), Sequelize.col('kredit'))), 'deposit']],
-          raw: true
-        }],
+        include: [
+          {
+            model: Journals,
+            attributes: [
+              [
+                Sequelize.fn(
+                  'SUM',
+                  (Sequelize.col('debet'),
+                  Sequelize.literal('-'),
+                  Sequelize.col('kredit'))
+                ),
+                'deposit'
+              ]
+            ],
+            raw: true
+          }
+        ]
       });
 
-      const payload = Object.assign({}, {
-        id: companyId,
-        month: month,
-        year: year,
-        total_salary: total_salary[0].dataValues.total_salaries,
-        deposit: deposits[0]['journals.deposit'],
-      });
+      const payload = Object.assign(
+        {},
+        {
+          id: companyId,
+          month: month,
+          year: year,
+          total_salary: total_salary[0].dataValues.total_salaries,
+          deposit: deposits[0]['journals.deposit']
+        }
+      );
       return res
         .status(200)
         .json(
           response(
             true,
             'Deposit summary has been successfully retrieved',
-            payload,
+            payload
           )
         );
-
     } catch (error) {
       if (error.errors) {
         return res.status(400).json(response(false, error.errors));
