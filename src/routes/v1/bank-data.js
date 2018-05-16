67275a2197a32be20b1eff8b1bfe5fd997c5cc7b
Dhanial Rizky <activatedfor@gmail.com>
2018-05-16 23:11:04 +0700

    hot fix bank route validation

--- src/routes/v1/bank-data.js
+++ src/routes/v1/bank-data.js
@@ -9,17 +9,13 @@ router.post(
   '/',
   [
     check('*.full_name')
-      .matches(/^([A-z]|\s)+$/i)
-      .withMessage('name should only has chars and space')
       .isLength({ min: 3 })
       .withMessage('name at least 3 letters'),
     check('*.bank_name', 'bank name should be present').exists(),
     check('*.bank_branch', 'bank branch should be present').exists(),
     check('*.account_number')
-      .matches(/^[^A-z]+([+]|\d|[-]\s)$/i)
-      .withMessage(
-        'should only account number format and no blank space in the end'
-      )
+      .matches(/^[\d]+$/i)
+      .withMessage('Only number that allowed')
       .isLength({ min: 5 })
       .withMessage('account number must be at least 5 chars long')
   ],
