11d6c5ed556bf0596d34be8378655c35e4e96a55
Dhanial Rizky <activatedfor@gmail.com>
2018-05-16 18:48:02 +0700

    #48 edit register and companies register to new specification

--- src/services/v1/companies/companies.service.js
+++ src/services/v1/companies/companies.service.js
@@ -1,22 +1,31 @@
 require('module-alias/register');
 const { response } = require('@helpers');
-const { companies: Company, digital_assets: DigitalAsset, employees: Employee, users: User, presences: Presence } = require('@models');
+const {
+  companies: Company,
+  digital_assets: DigitalAsset,
+  employees: Employee,
+  users: User,
+  presences: Presence
+} = require('@models');
 const Sequelize = require('sequelize');
 const { Op } = Sequelize;
 
 const companyService = {
-
   get: async (req, res) => {
     const presence_id = req.params.presence_id;
     try {
       const presences = await Presence.findOne({
         where: { id: presence_id },
-        include: [{
-          model: Employee,
-          include: [{
-            model: User
-          }]
-        }]
+        include: [
+          {
+            model: Employee,
+            include: [
+              {
+                model: User
+              }
+            ]
+          }
+        ]
       });
       const assets = await DigitalAsset.findOne({
         where: {
@@ -24,36 +33,34 @@ const companyService = {
           uploadable_type: 'employees'
         }
       });
-      let result = Object.assign(
-        {
-          'id': presences.id,
-          'presence_date': presences.presence_date,
-          'presence_start': presences.presence_id,
-          'presence_end': presences.presence_end,
-          'rest_start': presences.rest_start,
-          'rest_end': presences.rest_end,
-          'presence_overdue': presences.presence_overdue,
-          'is_absence': presences.is_absence,
-          'is_leave': presences.is_leave,
-          'overwork': presences.overwork,
-          'work_hours': presences.work_hours,
-          'salary': presences.salary,
-          'fine': presences.fine,
-          'employee': {
-            'id': presences.employee.id,
-            'role': presences.employee.role,
-            'full_name': presences.employee.user.full_name,
-            'email': presences.employee.user.email,
-            'phone': presences.employee.user.phone,
-            'assets': [
-              {
-                'type': assets.type,
-                'path': assets.path
-              }
-            ]
-          }
+      let result = Object.assign({
+        id: presences.id,
+        presence_date: presences.presence_date,
+        presence_start: presences.presence_id,
+        presence_end: presences.presence_end,
+        rest_start: presences.rest_start,
+        rest_end: presences.rest_end,
+        presence_overdue: presences.presence_overdue,
+        is_absence: presences.is_absence,
+        is_leave: presences.is_leave,
+        overwork: presences.overwork,
+        work_hours: presences.work_hours,
+        salary: presences.salary,
+        fine: presences.fine,
+        employee: {
+          id: presences.employee.id,
+          role: presences.employee.role,
+          full_name: presences.employee.user.full_name,
+          email: presences.employee.user.email,
+          phone: presences.employee.user.phone,
+          assets: [
+            {
+              type: assets.type,
+              path: assets.path
+            }
+          ]
         }
-      );
+      });
       if (!presences) {
         return res
           .status(400)
@@ -72,49 +79,82 @@ const companyService = {
 
   create: async (req, res) => {
     const { data } = req.body;
-    let noSpaces = req.body.data.name.replace(/\s/g, '');
     // res.local.users from auth middleware
     // check src/helpers/auth.js
+    const { id: user_id } = res.local.users;
 
     try {
       let finalCode;
-      let removePunctuations = noSpaces.replace(
-        /[.,/#!$%^&*;:{}=\-_`~()]/g,
-        ''
-      );
-      let theCode = removePunctuations.replace(/[aeiou]/gi, '').toUpperCase();
-      const companyExist = await Company.findOne({
-        order: [['created_at', 'DESC']],
-        where: { codename: { [Op.like]: `${theCode}%` } }
-      });
 
-      if (companyExist) {
-        let lastNums = companyExist.codename.substr(-3);
-        lastNums = parseInt(lastNums);
-        lastNums++;
-        lastNums = ('0000' + lastNums).substr(-3);
-        finalCode = theCode + '-' + lastNums;
-      } else {
-        const lastNum = '001';
-        finalCode = theCode + '-' + lastNum;
-      }
-
-      const payload = Object.assign({}, data, {
-        codename: finalCode,
-        active: true
-      });
-      let company = await Company.create(payload);
-      if (company) {
+      if (req.query.codename) {
+        const { codename } = req.query;
+        const isCompany = await Company.findOne({ where: { codename } });
+        if (!isCompany) {
+          return res
+            .status(400)
+            .json(response(false, 'Company code not found'));
+        }
+        const isInvited = await Employee.findOne({
+          where: { user_id, company_id: isCompany.id, flag: 1 }
+        });
+        if (!isInvited) {
+          return res
+            .status(400)
+            .json(response(false, 'You are not invited yet'));
+        }
+        await User.update(
+          { registration_complete: 1 },
+          { where: { id: user_id } }
+        );
         return res
           .status(200)
           .json(
-            response(
-              true,
-              'Company has been successfully created',
-              company,
-              null
-            )
+            response(true, 'Company has been successfully retrieved', isCompany)
           );
+      } else {
+        const codeName = data.name
+          .replace(/[aeiou.,/#!$%^&*;:{}=\-_`~()\s]/g, '')
+          .substring(0, 6)
+          .toUpperCase();
+        const companyExist = await Company.findOne({
+          order: [['created_at', 'DESC']],
+          where: { codename: { [Op.like]: `${codeName}%` } }
+        });
+
+        if (companyExist) {
+          let lastNums = companyExist.codename.substr(-3);
+          lastNums = parseInt(lastNums);
+          lastNums++;
+          lastNums = ('0000' + lastNums).substr(-3);
+          finalCode = codeName + '-' + lastNums;
+        } else {
+          const lastNum = '001';
+          finalCode = codeName + '-' + lastNum;
+        }
+
+        const payload = Object.assign({}, data, {
+          codename: finalCode,
+          active: true
+        });
+        const company = await Company.create(payload);
+        await Employee.create({
+          company_id: company.id,
+          salary: 0,
+          user_id,
+          role: 1,
+          flag: 2
+        });
+        await User.update(
+          { registration_complete: 1 },
+          { where: { id: user_id } }
+        );
+        if (company) {
+          return res
+            .status(201)
+            .json(
+              response(true, 'Company has been successfully created', company)
+            );
+        }
       }
     } catch (error) {
       if (error.errors) {
