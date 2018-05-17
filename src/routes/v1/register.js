57976bff070a64b10a49de48ec44963a4d570ea8
angling <angling.zainuddin@gmail.com>
2018-05-17 16:53:07 +0700

    #54 make validation for registration spec

--- src/routes/v1/register.js
+++ src/routes/v1/register.js
@@ -8,13 +8,23 @@ const { check, body, validationResult } = require('express-validator/check');
 router.post(
   '/',
   [
-    check('full_name', 'name should only has chars and space').isLength({
-      min: 4
-    }),
-    check('email')
+    check('full_name', 'name should only has chars and space')
+      .exists()
+      .isLength({
+        min: 4
+      })
+      .withMessage('full name must be at least 4 chars long'),
+    check('email', 'email should be present')
+      .exists()
       .isEmail()
       .withMessage('must be a valid email'),
-    check('birthday').isISO8601(),
+    check('phone', 'phone number should pe present')
+      .exists()
+      .matches(/^[\d]+$/i)
+      .withMessage('Only number that allowed'),
+    check('birthday')
+      .isISO8601()
+      .withMessage('birthday format should be YYYY-MM-DD'),
     check('password', 'passwords must be at least 5 chars long').isLength({
       min: 5
     })
