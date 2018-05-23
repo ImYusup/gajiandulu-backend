a8171a4a73b4997b3b52f3aaced495591cf07073
haryanapnx <hary.vai@gmail.com>
2018-05-24 02:45:32 +0700

    Create endpoint GET /me/deposit-summary?month={month}&year={year}

--- src/services/v1/me/me.service.js
+++ src/services/v1/me/me.service.js
@@ -13,6 +13,7 @@ const crypt = require('bcrypt');
 const path = require('path');
 const config = require('config');
 const fs = require('fs');
+const Sequelize = require('sequelize');
 
 const meService = {
   // find: async (req, res) => {
@@ -83,6 +84,98 @@ const meService = {
       return res
         .status(200)
         .json(response(true, 'Profile has been successfully updated'));
+    } catch (error) {
+      if (error.errors) {
+        return res.status(400).json(response(false, error.errors));
+      }
+      return res.status(400).json(response(false, error.message));
+    }
+  },
+
+  deposit: async (req, res) => {
+    const { id: userId } = res.local.users;
+    const { month, year } = req.query;
+
+    const employeeId = await Employee.findAll({
+      where: {
+        user_id: userId
+      },
+      attributes: ['id']
+    });
+
+    const getEmployeeId = employeeId[0].dataValues.id;
+
+    try {
+      const fines = await Presence.findAll({
+        where: { employee_id: getEmployeeId },
+        attributes: [[Sequelize.fn('SUM', Sequelize.col('fine')), 'fines']]
+      });
+
+      const workhour = await Presence.findAll({
+        where: { employee_id: getEmployeeId },
+        attributes: [
+          [Sequelize.fn('SUM', Sequelize.col('work_hours')), 'workhour']
+        ]
+      });
+
+      const presenceData = await Presence.findAll({
+        where: { employee_id: getEmployeeId },
+        attributes: [
+          'presence_date',
+          'presence_start',
+          'presence_end',
+          'rest_start',
+          'rest_end',
+          'presence_overdue',
+          'is_absence',
+          'is_leave',
+          'overwork',
+          'work_hours',
+          'fine'
+        ]
+      });
+
+      const salary = await Employee.findAll({
+        where: { user_id: userId },
+        attributes: [[Sequelize.fn('SUM', Sequelize.col('salary')), 'salaries']]
+      });
+
+      const employeeData = await Employee.findAll({
+        where: { user_id: userId },
+        attributes: ['flag']
+      });
+
+      const userData = await User.findAll({
+        where: { id: userId },
+        attributes: ['full_name', 'email', 'phone']
+      });
+
+      const result = {
+        id: userId,
+        full_name: userData[0]['full_name'],
+        email: userData[0]['email'],
+        phone: userData[0]['phone'],
+        flag: employeeData[0]['flag'],
+
+        salary_summary: {
+          month: month,
+          year: year,
+          total_salary: salary[0].dataValues.salaries,
+          fine: fines[0].dataValues.fines,
+          workhour: workhour[0].dataValues.workhour
+        },
+        presences: presenceData
+      };
+
+      return res
+        .status(200)
+        .json(
+          response(
+            true,
+            'Deposit summary list been successfully retrieved',
+            result
+          )
+        );
     } catch (error) {
       if (error.errors) {
         return res.status(400).json(response(false, error.errors));
